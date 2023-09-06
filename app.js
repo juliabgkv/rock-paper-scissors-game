a5abefabc3b881021e8dcbd198c5c5541db5995e
jbugakova <bugakovayk@gmail.com>
2023-09-06 13:12:02 +0300

    Added main functionality

---
+++ app.js
@@ -0,0 +1,48 @@
+const weapons = ['rock', 'scissors', 'paper'];
+const weaponButtons = document.querySelectorAll('.weapon-btn');
+const playerScoreElem = document.getElementById('playerScore');
+const pcScoreElem = document.getElementById('pcScore');
+const messageElem = document.getElementById('message');
+const playerChoice = document.getElementById('playerChoice');
+const pcChoice = document.getElementById('pcChoice');
+const score = {
+    player: 0,
+    pc: 0
+};
+
+weaponButtons.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+        const playerWeapon = e.target.dataset.weapon;
+        const pcWeaponIdx = Math.floor(Math.random() * 3);
+        const pcWeapon = weapons[pcWeaponIdx];
+    
+        playerChoice.dataset.weapon = playerWeapon;
+        pcChoice.dataset.weapon = pcWeapon;
+    
+        let isPlayerWin;
+        if(playerWeapon == pcWeapon) {
+            isPlayerWin = null;
+        } else if(playerWeapon == 'rock' && pcWeapon == 'scissors') {
+            isPlayerWin = true;
+        } else if(playerWeapon == 'scissors' && pcWeapon == 'paper') {
+            isPlayerWin = true;
+        } else if(playerWeapon == 'paper' && pcWeapon == 'rock') {
+            isPlayerWin = true;
+        } else {
+            isPlayerWin = false;
+        }
+    
+        let message;
+        if(isPlayerWin === null) {
+            message = 'It`s a tie!';
+        } else if(!isPlayerWin) {
+            message = 'You lose!';
+            pcScoreElem.textContent = ++score.pc;
+        } else if(isPlayerWin) {
+            message = 'You win!';
+            playerScoreElem.textContent = ++score.player;
+        }
+    
+        messageElem.textContent = message;
+    });
+});
